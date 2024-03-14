0cc2648bcd59e11ab20466c863af6c3f93d9228e
Tudor Toporas <tudortoporas@gmail.com>
2024-03-14 19:46:01 +0200

    refactored db wrapper

--- pythonProject1/src/bd.py
+++ pythonProject1/src/bd.py
@@ -1,9 +1,63 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 engine = create_engine('sqlite:///database.db')
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
+
+class Repository:
+    def __init__(self, model, sessionMaker):
+        self.__model = model
+        self.__sessionMaker = sessionMaker
+        self.session = None
+
+    @staticmethod
+    def of(model):
+        return Repository(model, Session)
+
+    def operation(func):
+        def inner(self, *args, **kwargs):
+            self.session = self.__sessionMaker()
+            try:
+                return func(self, *args, **kwargs)
+            except DBAPIError as e:
+                raise e
+            finally:
+                self.session.close()
+        return inner
+
+    @operation
+    def insert(self, obj):
+        self.session.add(obj)
+        self.session.commit()
+
+    @operation
+    def update(self, *query, update):
+        result = self._findAll(*query).first()[0]
+        for key in vars(update):
+            if not key.startswith('_'):
+                setattr(result, key, getattr(update, key))
+        self.session.commit()
+
+    def _findAll(self, *args):
+        return self.session.execute(self.session.query(self.__model).filter(*args))
+
+    @operation
+    def findAll(self, *args):
+        return list(map(lambda x: x[0], self._findAll(*args).fetchall()))
+
+    @operation
+    def delete(self, *args):
+        self.session.delete(self._findAll(*args).first()[0])
+        self.session.commit()
+
+    @operation
+    def deleteAll(self):
+        self.session.query(self.__model).delete()
+        self.session.commit()
+
 
 class Algorithm(Base):
     __tablename__ = 'algorithm'
@@ -13,12 +67,20 @@ class Algorithm(Base):
     framework = Column(String, nullable=False)
     symetric = Column(Boolean, nullable=False)
 
+    def __str__(self):
+        return f"{{\nid={self.id},\nname={self.name},\nframework={self.framework},\nsymetric={self.symetric}\n}}"
+
+    def __repr__(self):
+        return self.__str__().replace("\n", "")
+
+
 class File(Base):
     __tablename__ = 'file'
 
     id = Column(String, primary_key=True)
     bytes = Column(Integer, nullable=False)
     encrypted = Column(Boolean, nullable=False)
+
 
 class Key(Base):
     __tablename__ = 'key'
@@ -32,6 +94,7 @@ class Key(Base):
     file = relationship('File')
     algorithm = relationship('Algorithm')
 
+
 class PerformanceLogs(Base):
     __tablename__ = 'performance_logs'
 
@@ -43,123 +106,6 @@ class PerformanceLogs(Base):
 
     file = relationship('File')
     algorithm = relationship('Algorithm')
-
-def create_algorithm(id, name, framework, symetric):
-    session = Session()
-    algorithm = Algorithm(id=id, name=name, framework=framework, symetric=symetric)
-    session.add(algorithm)
-    session.commit()
-    session.close()
-
-def read_algorithm(id):
-    session = Session()
-    algorithm = session.query(Algorithm).filter(Algorithm.id == id).first()
-    session.close()
-    return algorithm
-
-def update_algorithm(id, name, framework, symetric):
-    session = Session()
-    algorithm = session.query(Algorithm).filter(Algorithm.id == id).first()
-    algorithm.name = name
-    algorithm.framework = framework
-    algorithm.symetric = symetric
-    session.commit()
-    session.close()
-
-def delete_algorithm(id):
-    session = Session()
-    algorithm = session.query(Algorithm).filter(Algorithm.id == id).first()
-    session.delete(algorithm)
-    session.commit()
-    session.close()
-
-def create_file(id, bytes, encrypted):
-    session = Session()
-    file = File(id=id, bytes=bytes, encrypted=encrypted)
-    session.add(file)
-    session.commit()
-    session.close()
-
-def read_file(id):
-    session = Session()
-    file = session.query(File).filter(File.id == id).first()
-    session.close()
-    return file
-
-def update_file(id, bytes, encrypted):
-    session = Session()
-    file = session.query(File).filter(File.id == id).first()
-    file.bytes = bytes
-    file.encrypted = encrypted
-    session.commit()
-    session.close()
-
-def delete_file(id):
-    session = Session()
-    file = session.query(File).filter(File.id == id).first()
-    session.delete(file)
-    session.commit()
-    session.close()
-
-def create_key(file_id, algorithm_id, isprivate, encryptionkey):
-    session = Session()
-    key = Key(file_id=file_id, algorithm_id=algorithm_id, isprivate=isprivate, encryptionkey=encryptionkey)
-    session.add(key)
-    session.commit()
-    session.close()
-
-def read_key(id):
-    session = Session()
-    key = session.query(Key).filter(Key.id == id).first()
-    session.close()
-    return key
-
-def update_key(id, file_id, algorithm_id, isprivate, encryptionkey):
-    session = Session()
-    key = session.query(Key).filter(Key.id == id).first()
-    key.file_id = file_id
-    key.algorithm_id = algorithm_id
-    key.isprivate = isprivate
-    key.encryptionkey = encryptionkey
-    session.commit()
-    session.close()
-
-def delete_key(id):
-    session = Session()
-    key = session.query(Key).filter(Key.id == id).first()
-    session.delete(key)
-    session.commit()
-    session.close()
-
-def create_performance_log(encoding_time, decoding_time, file_id, algorithm_id):
-    session = Session()
-    log = PerformanceLogs(encoding_time=encoding_time, decoding_time=decoding_time, file_id=file_id, algorithm_id=algorithm_id)
-    session.add(log)
-    session.commit()
-    session.close()
-
-def read_performance_log(id):
-    session = Session()
-    log = session.query(PerformanceLogs).filter(PerformanceLogs.id == id).first()
-    session.close()
-    return log
-
-def update_performance_log(id, encoding_time, decoding_time, file_id, algorithm_id):
-    session = Session()
-    log = session.query(PerformanceLogs).filter(PerformanceLogs.id == id).first()
-    log.encoding_time = encoding_time
-    log.decoding_time = decoding_time
-    log.file_id = file_id
-    log.algorithm_id = algorithm_id
-    session.commit()
-    session.close()
-
-def delete_performance_log(id):
-    session = Session()
-    log = session.query(PerformanceLogs).filter(PerformanceLogs.id == id).first()
-    session.delete(log)
-    session.commit()
-    session.close()
 
 
 Base.metadata.create_all(engine)
--- pythonProject1/src/insert_exemple.py
+++ pythonProject1/src/insert_exemple.py
@@ -1,31 +1,33 @@
 from src.bd import *
 
-def delete_all(session, Model):
-    session.query(Model).delete()
-    session.commit()
+algRepo = Repository.of(Algorithm)
+fileRepo = Repository.of(File)
+keyRepo = Repository.of(Key)
+perfRepo = Repository.of(PerformanceLogs)
+
 
 def delete_all_records():
-    session = Session()
-    delete_all(session, Algorithm)
-    delete_all(session, File)
-    delete_all(session, Key)
-    delete_all(session, PerformanceLogs)
-    session.close()
+    algRepo.deleteAll()
+    fileRepo.deleteAll()
+    keyRepo.deleteAll()
+    perfRepo.deleteAll()
+
 
 delete_all_records()
 
-create_algorithm(id="1", name="AES", framework="PyCrypto", symetric=True)
-create_algorithm(id="2", name="RSA", framework="OpenSSL", symetric=False)
-create_algorithm(id="3", name="SHA-256", framework="Hashlib", symetric=False)
+algRepo.insert(Algorithm(id="1", name="AES", framework="PyCrypto", symetric=True))
+algRepo.insert(Algorithm(id="2", name="RSA", framework="OpenSSL", symetric=False))
+algRepo.insert(Algorithm(id="3", name="SHA-256", framework="Hashlib", symetric=False))
+
+fileRepo.insert(File(id="file1", bytes=1024, encrypted=True))
+fileRepo.insert(File(id="file2", bytes=2048, encrypted=False))
+
+keyRepo.insert(Key(file_id="file1", algorithm_id="1", isprivate=True, encryptionkey="random_key_1"))
+keyRepo.insert(Key(file_id="file2", algorithm_id="2", isprivate=False, encryptionkey="random_key_2"))
 
 
-create_file(id="file1", bytes=1024, encrypted=True)
-create_file(id="file2", bytes=2048, encrypted=False)
-
-
-create_key(file_id="file1", algorithm_id="1", isprivate=True, encryptionkey="random_key_1")
-create_key(file_id="file2", algorithm_id="2", isprivate=False, encryptionkey="random_key_2")
-
-
-
-
+algRepo.insert(Algorithm(id="4", name="TwoFish", framework="PyCrypto", symetric=True))
+algRepo.delete(Algorithm.id == "4")
+print(algRepo.findAll(Algorithm.framework == "PyCrypto"))
+algRepo.update(Algorithm.id == 1, update=Algorithm(name="TwoFish"))
+print(algRepo.findAll(Algorithm.framework == "PyCrypto"))
