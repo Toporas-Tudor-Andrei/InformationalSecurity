51d2c0d9e8e5ec6c5942abae5ab18885ad214a1b
Tudor Toporas <tudortoporas@gmail.com>
2024-03-31 11:16:05 +0300

    performance logs acquisition and processing

--- pythonProject1/CryptoWrapper/CryptoWrapper.py
+++ pythonProject1/CryptoWrapper/CryptoWrapper.py
@@ -23,9 +23,62 @@ def getFrameworks():
     return set(map(lambda x: x.framework, algRepo.findAll()))
 
 
+def getFrameworkByAlgorithm(algorithm):
+    return set(map(lambda x: x.framework, algRepo.findAll(Algorithm.name == algorithm)))
+
+
+def perfData(fileId=None, /, *, alg=None, framework=None):
+    """
+    Query function for finding the representative performance rows in the database
+    :param fileId: filter parameter for finding a certain file's data
+    :param alg: filter parameter for algorithm name
+    :param framework: filter parameter for framework name
+    :return: a list of PerformanceLogs objects corresponding to the filters given as parameters
+    """
+    filters = [getattr(Algorithm, col) == val for col, val in
+               [("name", alg), ("framework", framework)] if val is not None]
+    ids = list(map(lambda it: it.id, algRepo.findAll(*filters)))
+
+    file_cond = [getattr(PerformanceLogs, col) == val for col, val in
+                 [("file_id", fileId)] if val is not None]
+
+    return perfRepo.findAll(*file_cond, PerformanceLogs.algorithm_id.in_(ids))
+
+
+def logsProcessing(logs, operation, target):
+    """
+    Applies certain reductions on the performance logs gives as input
+    :param logs: a list of PerformanceLogs objects to be aggregated
+    :param operation: reduction method can take the values: "avg", "max", "min", "" - for no reduction
+    :param target: slice of data to be used in the reduction can take the values:
+        "enc", "dec", "diff" - difference between encoding and decoding time, default "enc"
+    :return: the result of the reduction operation
+    """
+    if target == "enc":
+        data = list(map(lambda it: it.encoding_time, logs))
+    elif target == "dec":
+        data = list(map(lambda it: it.decoding_time, logs))
+    elif target == "diff":
+        data = list(map(lambda it: it.encoding_time - it.decoding_time, logs))
+    else:
+        data = list(map(lambda it: it.encoding_time, logs))
+
+    if operation == "avg":
+        return sum(data)/len(data)
+    elif operation == "min":
+        min(data)
+    elif operation == "max":
+        max(data)
+    else:
+        return data
+
 
 if __name__ == "__main__":
     print(algRepo.findAll())
     print(getAlgorithms())
     print(getAlgorithmByFramework("OpenSSL"))
     print(getFrameworks())
+    print(getFrameworkByAlgorithm("AES"))
+    print(algRepo.findAll(Algorithm.name.in_(["AES", "DES"])))
+    print(perfData())
+    print(perfRepo.findAll())
